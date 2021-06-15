8f99082eb91066c92ec1a442664d8a65bf81ff58
jacoblovins <lovins.jacob@yahoo.com>
2021-06-15 07:59:46 -0400

    initial setup

---
+++ src/test/java/com/example/expense/SpringExpense/SpringExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expense.SpringExpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
